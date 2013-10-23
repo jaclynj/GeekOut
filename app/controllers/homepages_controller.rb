db0e5031f953c9422dae5e291aee1dc4bc306267
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-23 12:17:02 -0400

    filter by location works properly

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,26 +1,43 @@
 class HomepagesController < ApplicationController
 
-def view
-end
+  def view
+  end
 
   def results
-    filter = params[:tag]
-    @filtered = []
+    #filters by tags
+    tag_filter = params[:tag]
+    @filtered_by_tag = []
     @filtered_multiple_tags = []
     @filtered_at_least_one_tag = []
-    if filter != nil
-      filter.each do |f|
-        @this_tag = Tagging.where({tag_id: f})
-        @this_tag.each do |t|
+
+    if tag_filter != nil
+      tag_filter.each do |f|
+        @filtered_by_single_tag = Tagging.where({tag_id: f})
+        @filtered_by_single_tag.each do |t|
           id = t.place_id
-          @filtered_place = Place.find_by_id(id)
-          @filtered << @filtered_place
+          @filtered_by_single_tag = Place.find_by_id(id)
+          @filtered_by_tag << @filtered_by_single_tag
         end
       end
-     @filtered_at_least_one_tag = @filtered.uniq
-     @filtered_multiple_tags = @filtered.select{|p| @filtered.count(p) > 1}
-     @filtered_multiple_tags.uniq!
-   end
-end
+    end
+    #filters by location
+    city = params[:location]
+    if city == 'Any City'
+    else
+      @filtered_by_location = Place.where({location: city})
+      if @filtered_by_location != []
+        @filtered_by_tag.each do |outing|
+          if @filtered_by_location.include?(outing)
+          else
+            @filtered_by_tag.delete(outing)
+          end
+        end
+      end
+    end
+    #contains outings matching at least one tag & multiple tags
+    @filtered_at_least_one_tag = @filtered_by_tag.uniq
+    @filtered_multiple_tags = @filtered_by_tag.select{|p| @filtered_by_tag.count(p) > 1}
+    @filtered_multiple_tags.uniq!
+  end
 
 end
