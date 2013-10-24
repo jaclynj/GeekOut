24ca53908c7c7bce85f9bbd19f59a39073d73aed
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-24 13:22:41 -0400

    edited nav, styling

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,9 @@ class Tag < ActiveRecord::Base
   attr_accessible :name, :places
   has_many :taggings
   has_many :places, through: :taggings
+
+  def current_places
+    self.places.where("date > ?", Date.today)
+  end
+
 end
