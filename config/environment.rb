1c1707c6b09ffc910b84d17c454077ab81491ea7
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-25 08:27:34 -0400

    modified config enviornment to see if it will unbreak heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-config.gem "httparty"
+
 # Initialize the rails application
 IntoverseApp::Application.initialize!
